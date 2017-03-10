7bc056fa84b7375c50eebd23651ff04cbb41ab5f
Kanazawa Nobuyoshi <nobunobusokuhou@gmail.com>
2017-03-10 15:25:25 +0900

    サウンドのInit関数の呼び出し修正
    DirectX.cppのAppInit関数ところの呼び出しを統一しました。

--- Sound.h
+++ Sound.h
@@ -30,28 +30,28 @@ public:
 		return inst;
 	}
 
-	HRESULT			InitSound();
-	int						LoadSound(char* szFileName);
-	void					PlaySound(int iSoundIndex, bool LoopFlag);
-	void					SoundSet();
+	HRESULT			Init();															// サウンドの初期化
 
-	void					SE_play(string SE_mode);			// SE再生
-	void					SE_stop(string SE_mode);			// SE停止
-	void					BGM_play(string BGM_mode);		// BGM再生
-	void					BGM_stop(string BGM_mode);		// BGM停止
+	void					PlaySE(string name);										// SE再生
+	void					StopSE(string name);										// SE停止
+	void					PlayBGM(string name);									// BGM再生
+	void					StopBGM(string name);									// BGM停止
 	
-	IXAudio2*								m_XAudio2;
-	IXAudio2MasteringVoice*		m_MasteringVoice;
-	IXAudio2SourceVoice*			m_SourceVoice[max_wav];
-	
-	BYTE*			m_WavBuffer[max_wav];				//波形データ（フォーマット等を含まない、純粋に波形データのみ）
-	DWORD			m_WavSize[max_wav];					//波形データのサイズ
-
-	int					m_SoundCurrentIndex;					//音の現在の再生番号
-	bool				m_Flag;											//BGMがなってるか
-
 private:
 	Sound();
-	virtual ~Sound();
-};
+	~Sound();
+	void					Set();															// サウンドのロード
+	void					PlaySound(int soundIndex, bool loopFlag);		// サウンドの再生
+	int						Load(char* szFileName);								// wavファイルの読み込み
 
+	BYTE*				m_wavBuffer[max_wav];								//波形データ（フォーマット等を含まない、純粋に波形データのみ）
+	DWORD				m_wavSize[max_wav];									//波形データのサイズ
+
+	int						m_soundCurrentIndex;									//音の現在の再生番号
+	bool					m_flag;															//BGMがなってるか
+
+	IXAudio2*								m_audio2;
+	IXAudio2MasteringVoice*		m_masteringVoice;
+	IXAudio2SourceVoice*			m_sourceVoice[max_wav];
+
+};
